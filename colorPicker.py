0bc6fc01044ee9af81a9dfd2bb8c203ccf340667
Gingernut08 <oliver.spam.08@gmail.com>
2026-06-13 12:39:05 +0100

    Finalised picker and moved to a seperate file to import to main.py

---
+++ colorPicker.py
@@ -0,0 +1,83 @@
+import colorsys, pygame
+
+def to_8Bit_RGB(color):
+    return [255 * i for i in color]
+
+class ColorSlider:
+    def __init__(self, pos, width, height, type, group, screen):
+        # sliders.append(self)
+        self.width = width
+        self.height = height
+        self.pos = pos
+        self.focused = False
+        self.value = 0
+        self.type = type
+        self.group = group
+        self.screen = screen
+
+    def is_clicked(self, pos):
+        return self.pos[0] <= pos[0] <= self.pos[0] + self.width and self.pos[1] <= pos[1] <= self.pos[1] + self.height
+
+    def draw(self):
+        self.colors = [i / self.width for i in range(self.width)]
+        self.hlsColor = self.group.hlsColor
+        for i in range(self.width):
+            self.hlsColor[self.type] = self.colors[i]
+            self.rgbColor = to_8Bit_RGB(colorsys.hls_to_rgb(*self.hlsColor))
+            pygame.draw.rect(self.screen, (self.rgbColor), pygame.Rect(self.pos[0] + i, self.pos[1], 1, self.height))
+        pygame.draw.rect(self.screen, (255, 255, 255), pygame.Rect(self.pos[0] + self.width * self.value, self.pos[1], 1, self.height))
+
+    def event_handle(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            if self.is_clicked(pos):
+                self.focused = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            self.focused = False
+    
+    def update_position(self, pos):
+        if self.focused:
+            self.value = min(max((pos[0] - self.pos[0]) / self.width, 0), 1)
+
+class ColorPicker:
+    def __init__(self, screen):
+        self.pos = [100, 100]
+        self.sliderWidth = 300
+        self.sliderHeight = 20
+        self.sliderSpacing = 10
+        self.width = self.sliderWidth + self.sliderHeight * 3 + self.sliderSpacing * 3 + 2 * self.sliderSpacing
+        self.height = self.sliderHeight * 3 + self.sliderSpacing * 2 + 2 * self.sliderSpacing
+        self.sliderPos = [[self.sliderSpacing + self.pos[0], self.sliderSpacing + self.pos[1] + i * (self.sliderHeight + self.sliderSpacing)] for i in range(3)]
+        self.types = [0, 1, 2]
+        self.sliders = [ColorSlider(self.sliderPos[i], self.sliderWidth, self.sliderHeight, self.types[i], self, screen) for i in range(len(self.sliderPos))]
+        self.hlsColor = [0, 0, 0]
+        self.rgbColor = colorsys.hsv_to_rgb(*self.hlsColor)
+        self.font = pygame.font.SysFont("mscomicsans", 22)
+        self.screen = screen
+    
+    def event_handle(self, event):
+        for slider in self.sliders:
+            slider.event_handle(event)
+        
+    
+    # def is_submit_clicked(self, pos):
+    #     if 
+    def update_position(self, pos):
+        for slider in self.sliders:
+            slider.update_position(pos)
+    
+    def update_color(self):
+        self.hlsColor = [slider.value for slider in self.sliders]
+        self.rgbColor = colorsys.hls_to_rgb(*self.hlsColor)
+    
+    def draw(self):
+        pygame.draw.rect(self.screen, (50, 50, 50), pygame.Rect(self.pos[0], self.pos[1], self.width, self.height))
+        renderText = self.font.render("Submit", 1, (0, 0, 0))
+        textPos = (self.pos[0] + self.sliderWidth + self.sliderSpacing * 3 + self.sliderHeight * 1.5, self.pos[1] + self.sliderHeight * 2.5 + self.sliderSpacing * 3 + 2)
+        textRect = renderText.get_rect(center = textPos)
+        for slider in self.sliders:
+            self.update_color()
+            slider.draw()
+        pygame.draw.rect(self.screen, to_8Bit_RGB(self.rgbColor), pygame.Rect(self.pos[0] + self.sliderWidth + self.sliderSpacing * 2, self.pos[1] + self.sliderSpacing, self.sliderHeight * 3 + self.sliderSpacing * 2, self.sliderHeight * 2 + self.sliderSpacing))
+        pygame.draw.rect(self.screen, (200, 200, 200), pygame.Rect(self.pos[0] + self.sliderWidth + self.sliderSpacing * 2, self.pos[1] + self.sliderHeight * 2 + self.sliderSpacing * 3, self.sliderHeight * 3 + self.sliderSpacing * 2, self.sliderHeight))
+        self.screen.blit(renderText, textRect)
--- main.py
+++ main.py
@@ -1,72 +1,8 @@
-import colorsys, pygame
+import pygame
+from colorPicker import ColorPicker
 
 pygame.init()
-
-class Slider:
-    def __init__(self, pos, width, height, type, group):
-        # sliders.append(self)
-        self.width = width
-        self.height = height
-        self.pos = pos
-        self.focused = False
-        self.value = 0
-        self.type = type
-        self.group = group
-
-    def is_clicked(self, pos):
-        return self.pos[0] <= pos[0] <= self.pos[0] + self.width and self.pos[1] <= pos[1] <= self.pos[1] + self.height
-
-    def draw(self):
-        self.colors = [i / self.width for i in range(self.width)]
-        self.hlsColor = self.group.hlsColor
-        for i in range(self.width):
-            self.hlsColor[self.type] = self.colors[i]
-            self.rgbColor = to_8Bit_RGB(colorsys.hls_to_rgb(*self.hlsColor))
-            pygame.draw.rect(screen, (self.rgbColor), pygame.Rect(self.pos[0] + i, self.pos[1], 1, self.height))
-        pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(self.pos[0] + self.width * self.value, self.pos[1], 1, self.height))
-
-    def event_handle(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            if self.is_clicked(pos):
-                self.focused = True
-        if event.type == pygame.MOUSEBUTTONUP:
-            self.focused = False
-    
-    def update_position(self, pos):
-        if self.focused:
-            self.value = min(max((pos[0] - self.pos[0]) / self.width, 0), 1)
-
-class ColorPicker:
-    def __init__(self):
-        self.pos = [100, 100]
-        self.sliderWidth = 300
-        self.sliderHeight = 20
-        self.sliderSpacing = 10
-        self.sliderPos = [[self.pos[0], self.pos[1] + i * (self.sliderHeight + self.sliderSpacing)] for i in range(3)]
-        self.types = [0, 1, 2]
-        self.sliders = [Slider(self.sliderPos[i], self.sliderWidth, self.sliderHeight, self.types[i], self) for i in range(len(self.sliderPos))]
-        self.hlsColor = [0, 0, 0]
-        self.rgbColor = colorsys.hsv_to_rgb(*self.hlsColor)
-    
-    def event_handle(self, event):
-        for slider in self.sliders:
-            slider.event_handle(event)
-    
-    def update_position(self, pos):
-        for slider in self.sliders:
-            slider.update_position(pos)
-    
-    def update_color(self):
-        self.hlsColor = [slider.value for slider in self.sliders]
-        self.rgbColor = colorsys.hls_to_rgb(*self.hlsColor)
-    
-    def draw(self):
-        for slider in self.sliders:
-            self.update_color()
-            slider.draw()
-        pygame.draw.rect(screen, to_8Bit_RGB(self.rgbColor), pygame.Rect(self.pos[0] + self.sliderWidth + self.sliderSpacing, self.pos[1], self.sliderHeight * 3 + self.sliderSpacing * 2, self.sliderHeight * 3 + self.sliderSpacing * 2))
-
+pygame.font.init()
 
 WIDTH, HEIGHT = 1920, 1080
 
@@ -74,14 +10,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
 
 running = True
 
-hlsColor = [0, 0.5, 0.5]
-rgbColor = colorsys.hls_to_rgb(*hlsColor)
-step = 0.001
-
-colorPick = ColorPicker()
-
-def to_8Bit_RGB(color):
-    return [255 * i for i in color]
+colorPick = ColorPicker(screen)
 
 while running:
     for event in pygame.event.get():
@@ -92,28 +21,7 @@ while running:
                 running = False
         colorPick.event_handle(event)
     colorPick.update_position(pygame.mouse.get_pos())
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_q]:
-        hlsColor[0] += step
-        hlsColor[0] %= 1
-    if keys[pygame.K_a]:
-        hlsColor[0] -= step
-        hlsColor[0] %= 1
-    if keys[pygame.K_w]:
-        hlsColor[1] += step
-        hlsColor[1] = max(min(hlsColor[1], 1), 0)
-    if keys[pygame.K_s]:
-        hlsColor[1] -= step
-        hlsColor[1] = max(min(hlsColor[1], 1), 0)
-    if keys[pygame.K_e]:
-        hlsColor[2] += step
-        hlsColor[2] = max(min(hlsColor[2], 1), 0)
-    if keys[pygame.K_d]:
-        hlsColor[2] -= step
-        hlsColor[2] = max(min(hlsColor[2], 1), 0)
-    rgbColor = colorsys.hls_to_rgb(*hlsColor)
-    screen.fill((to_8Bit_RGB(rgbColor)))
+    screen.fill((255, 255, 255))
     colorPick.draw()
-    # print(testSlider.value)
     pygame.display.flip()
 pygame.quit()
