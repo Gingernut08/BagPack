e207c7141820ec3f71a6f4f6e9fb84b2e0bc39e4
Gingernut08 <oliver.spam.08@gmail.com>
2026-06-19 10:19:38 +0100

    Moved file location of some code

--- buttonFunctions.py
+++ buttonFunctions.py
@@ -2,10 +2,6 @@ from imports import tkinter, json, os
 from item import create_item, new_item, focus_pygame_window
 from guiElements import Button, TextInput
 from varSetup import new_item_makers, items, WIDTH, HEIGHT, screen, leftSpacing, topSpacing, bottomSpacing, numLeftButtons, numRightButtons, itemColor, baseDir
-<<<<<<< HEAD
-=======
-from generalFuncs import random_color
->>>>>>> 9d32714ba088244960a382e41c4ef9a3bc6bb145
 
 def create_new_item_picker():
     xPos = leftSpacing * 2
@@ -26,6 +22,9 @@ def create_new_item_picker():
     j += 1
     button = Button((xPos + j * 250, topSpacing + i * 100), 150, 60, "SUBMIT", submit_item, None, screen)
     new_item_makers.append(button)
+    
+    for new_item_maker in new_item_makers:
+        new_item_maker.shown = False
 
 def create_buttons():
     i = 0
@@ -42,6 +41,9 @@ def create_buttons():
     button = Button((0, topSpacing + i * (HEIGHT - topSpacing - bottomSpacing) // numLeftButtons), 200, (HEIGHT - topSpacing - bottomSpacing) // numLeftButtons, "IMPORT", import_item, (screen, None), screen)
     i += 1
     button = Button((0, topSpacing + i * (HEIGHT - topSpacing - bottomSpacing) // numLeftButtons), 200, (HEIGHT - topSpacing - bottomSpacing) // numLeftButtons, "NEW", new_item, None, screen)
+    
+for item in items:
+    item.shown = True
 
 def cancel():
     for new_item_maker in new_item_makers:
--- main.py
+++ main.py
@@ -8,33 +8,12 @@ pygame.init()
 pygame.font.init()
 root.withdraw()
 
-commonKeys = [
-    "NAME", "BRAND", "WEIGHT", "COLOR", "CATEGORY"
-    ]
-testItemPeramters = {   
-                        "COMMON":{
-                            "NAME": "Atmos AG 65",
-                            "BRAND": "Osprey",
-                            "WEIGHT": 2.09,
-                            "COLOR": "Blue",
-                            "CATEGORY": "Bag"
-                        },
-                        "SPECIALIST":{
-                            "CAPACITY": "65 Litres"
-                        }
-                    }
-
 running = True
 
 recall_item_states(screen)
 
 create_buttons()
 create_new_item_picker()
-
-for new_item_maker in new_item_makers:
-    new_item_maker.shown = False
-for item in items:
-    item.shown = True
 
 while running:
     tabPress = False
@@ -45,13 +24,6 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-            if event.key == pygame.K_1:
-                for new_item_maker in new_item_makers:
-                    new_item_maker.shown = not new_item_maker.shown
-                    
-            if event.key == pygame.K_2:
-                for item in items:
-                    item.shown = not item.shown
 
             tabPress = cycle_text_boxes(event, tabPress)
 
